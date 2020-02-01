e7bf27a318234e4a94444c868be4dfa8cb2c294c
animacpac <56101332+animacpac@users.noreply.github.com>
2020-02-01 11:41:49 -0700

    New Change
    Changes

--- HelloWorld/src/barreto/Hello.java
+++ HelloWorld/src/barreto/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 
     public void sayHello(String name) {
-        System.out.println("Hello Team 2 Amy, John, Carson "+name+" !");
+        System.out.println("Hello Team 2 Amy, John, Carson my name is "+name+" !");
     }
     public static void main (String[] args){
         Hello  hello = new Hello();
