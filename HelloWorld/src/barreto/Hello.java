326af501f804285ac6e2f128db0056cd9a4d9787
animacpac <56101332+animacpac@users.noreply.github.com>
2020-02-01 11:40:45 -0700

    Changes
    Just chaging

--- HelloWorld/src/barreto/Hello.java
+++ HelloWorld/src/barreto/Hello.java
@@ -4,11 +4,11 @@ public class Hello {
 
 
     public void sayHello(String name) {
-        System.out.println("Hello World "+name+" !");
+        System.out.println("Hello Team 2 Amy, John, Carson "+name+" !");
     }
     public static void main (String[] args){
         Hello  hello = new Hello();
-        hello.sayHello(name: "valter");
+        hello.sayHello("valter");
 
     }
 }
